de803a84f6a4717d06f506bcbd21f25ca0bf2e02
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-07-26 15:33:07 +0700

    Update crp.sh

--- crp.sh
+++ crp.sh
@@ -1,6 +1,2 @@
-apt-get -y update
-apt-get install screen
-sudo wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
-sudo dpkg -i uam-latest_amd64.deb
 cd /opt/uam
 screen ./uam --pk 57D4B88E6BC80DD524DD5CE27F6CD0E48CECB3B67CAE169BC91E698706F22B0D
